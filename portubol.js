f1ab3b49e6721302e04301d62f72ed6210d2649c
Henrique Silva <henrique.fibonacci@hotmail.com>
2022-08-28 22:23:13 -0300

    feat: Implementação de documentação com portubol-js

--- portubol.js
+++ portubol.js
@@ -6,24 +6,192 @@
 
 pt = {
 
-    adicionar_evento:  function( obj , type, fn ){ obj.addEventListener(type, fn), false },
+    alerta_navegador: function(msg){ 
+                if ( typeof( msg ) == 'function' ){
+                    alert( msg() )
+                } else {
+                    alert( msg ) 
+                }
+        },
+
+    adicionar_evento:      function( obj, type, fn ) { obj.addEventListener(type, fn), false },
+
+    adicionar_evento_doc:  function( type, fn ) { document.addEventListener(type, fn), false },
+
+    selecionar_elemento:   function( obj ) { return document.querySelector(obj) },
+
+    selecionar_elementos:  function( obj ) { return document.querySelectorAll(obj) },
     
-    remover_evento:    function( obj , type, fn ){ obj.removeEventListener(type, fn), false },
+    remover_evento:        function( obj , type, fn ) { obj.removeEventListener(type, fn), false },
     
-    ao_abrir_a_pagina: function( fn ){ pt.adicionar_evento(document, 'DOMContentLoaded', fn) },
+    ao_abrir_a_pagina:     function( fn ) { pt.adicionar_evento(document, 'DOMContentLoaded', fn) },
     
-    adicionar_classe:  function( obj , className ) { 
-        if(typeof(className)=='string' && className.split(' ').length==1) { 
+    adicionar_classe:      function( obj , className ) {
+        
+        if(obj.target){
+            
+            obj = "#"+obj.target.id
+            
             document.querySelector(obj).classList.add(className) 
+
+            if(typeof(className)=='string' && className.split(' ').length==1) { 
+        
+                document.querySelector(obj).classList.add(className) 
+            
+            }
+            
+            else if(typeof(className)=='string' && className.split(' ').length>1) {
+            
+                var classNames = className.split(' ');
+            
+                classNames.map((objs)=>{
+                    document.querySelector(obj).classList.add(objs); 
+                })
+            
+            }
+    
+            else if(typeof(className) == 'function'){
+            
+                var retorno = className;
+            
+                document.querySelector(obj).classList.add(retorno())
+    
+            }
+
+        }
+
+        
+        if(typeof(className)=='string' && className.split(' ').length==1) { 
+        
+            document.querySelector(obj).classList.add(className) 
+        
         }
         
-        if(typeof(className)=='string' && className.split(' ').length>1) {
+        else if(typeof(className)=='string' && className.split(' ').length>1) {
+        
             var classNames = className.split(' ');
+        
             classNames.map((objs)=>{
                 document.querySelector(obj).classList.add(objs); 
             })
+        
+        }
+
+        else if(typeof(className) == 'function'){
+        
+            var retorno = className;
+        
+            document.querySelector(obj).classList.add(retorno())
+
         }
     
     },
+
+
+    remover_classe:      function( obj , className ) {
+        
+        console.log(obj.target)
+        
+        if(obj.id){
+            obj = "#"+obj.id
+
+            document.querySelector(obj).classList.remove(className) 
+
+            if(typeof(className)=='string' && className.split(' ').length==1) { 
+        
+                document.querySelector(obj).classList.remove(className) 
+            
+            }
+            
+            else if(typeof(className)=='string' && className.split(' ').length>1) {
+            
+                var classNames = className.split(' ');
+            
+                classNames.map((objs)=>{
+                    document.querySelector(obj).classList.remove(objs); 
+                })
+            
+            }
+    
+            else if(typeof(className) == 'function'){
+            
+                var retorno = className;
+            
+                document.querySelector(obj).classList.remove(retorno())
+    
+            }
+
+        }
+
+
+
+
+        if(obj.target){
+            
+            obj = "#"+obj.target.id
+            
+            document.querySelector(obj).classList.remove(className) 
+
+            if(typeof(className)=='string' && className.split(' ').length==1) { 
+        
+                document.querySelector(obj).classList.remove(className) 
+            
+            }
+            
+            else if(typeof(className)=='string' && className.split(' ').length>1) {
+            
+                var classNames = className.split(' ');
+            
+                classNames.map((objs)=>{
+                    document.querySelector(obj).classList.remove(objs); 
+                })
+            
+            }
+    
+            else if(typeof(className) == 'function'){
+            
+                var retorno = className;
+            
+                document.querySelector(obj).classList.remove(retorno())
+    
+            }
+
+        }
+
+        
+        if(typeof(className)=='string' && className.split(' ').length==1) { 
+        
+            document.querySelector(obj).classList.remove(className) 
+        
+        }
+        
+        else if(typeof(className)=='string' && className.split(' ').length>1) {
+        
+            var classNames = className.split(' ');
+        
+            classNames.map((objs)=>{
+                document.querySelector(obj).classList.remove(objs); 
+            })
+        
+        }
+
+        else if(typeof(className) == 'function'){
+        
+            var retorno = className;
+        
+            document.querySelector(obj).classList.remove(retorno())
+
+        }
+    
+    },
+
+
+
+
+    a_cada: function(obj, fn){
+        pt.selecionar_elementos(obj).forEach(e =>{
+            fn(e);
+        })
+    }
 
 }
